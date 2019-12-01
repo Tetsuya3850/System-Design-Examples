c090c55cb3b2cd185d035fe055eb457eeb819804
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-12-01 16:22:19 +0900

    refactored gateway app

--- netflix/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ netflix/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.example.gateway;
 import com.example.gateway.filters.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
